a1dae3132a95c32d6abd6460999d4c11f6ee8b87
Vinicius <vinicius.amorim@stitcloud.com>
2022-05-18 18:32:53 -0300

    Server done

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter'
 
-import { prisma } from './prisma'
 import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedback-repository'
 import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case'
 
